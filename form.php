a2f3b0f7d71cbbc6b61889e93e427474815bf65b
Nem3sisAlpha <xavierbriand31@gmail.com>
2023-10-14 22:19:05 -0400

    update 5.10

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
 <body>
 
 <p>Welcome <?=  $_POST["name"]; ?><br></p>
-<p>This Email address <?=  $_POST["name"]; ?> was entered prior to the access of this page<br></p>
+<p>This Email address "<?=  $_POST["email"]; ?>" was entered prior to the access of this page<br></p>
 
 </body>
 </html>

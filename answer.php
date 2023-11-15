ff6b16a9db04007cd8be39131c4bc525267f0760
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2023-11-15 13:20:08 -0500

    Update answer.php

--- answer.php
+++ answer.php
@@ -1,1 +1,16 @@
+<!DOCTYPE html>
 
+<html>
+    <head>
+        <title>SQL - Results</title>
+        <?php 
+            $rownum = (int)($_GET["pictureinfo"]);
+    </head>
+
+    <body>
+
+
+
+      
+    </body>
+</html>

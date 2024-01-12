cea9ac4e288b881d87491b0e77ecd3c45f694e65
Nem3sisAlpha <xavierbriand31@gmail.com>
2024-01-12 11:42:51 -0500

    test

--- index.php
+++ index.php
@@ -9,9 +9,6 @@
             $password = "password";
             $database = "Pictures_Taken";
             $conn = mysqli_connect($server, $username, $password, $database);
-            
-            $raw = `./bme280/bme280`;
-            $deserialized = json_decode($raw, true);
 
             // Check for successful connection
             if (!$conn) {
@@ -25,12 +22,18 @@
     </head>
     <body>
         <h1>HALT</h1>
-        <img src="https://github.com/Nem3sisAlpha/XavierBriandWebServers/blob/main/Images%20Lab%201/VAX05085.jpg?raw=true" width="800" height="1200"/>
         <h2>You must be judged by the judging bird before entering this website</h2>
 
         <h3>Recorded amount of pictures taken monthly per year</h3>
         <form action="result.php" method="get">
-        <label for="pictures">Select Month to see the amount of pictures taken per year:</label><br/>
+
+        <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <input type="text" placeholder="Search..">
+        </div>
+
         <select id="pictureinfo" name="pictureinfo">
             <?php
                 foreach($result as $row) 

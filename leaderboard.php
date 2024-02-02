addcb8a5c7d27c7854113938e8e0b28129514bdf
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-02 12:17:50 -0500

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Leaderboard</title>
+    </head>
+  
+    <body>
+      <h2>Simon Says Leaderboard</h2>
+      <table>
+        <tr>
+          <td>Rank</td>
+          <td>Name</td>
+          <td>Score</td>
+        </tr>
 
+        <?php 
+          $server = "localhost";
+          $username = "leader";
+          $password = "password";
+          $database = "point_score";
+          $conn = mysqli_connect($server, $username, $password, $database);
+
+          // Check for successful connection
+          if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+          }
+            
+          $sql = "select name, score from leaderboard order by score desc;";
+          $result = mysqli_query($conn, $sql);
+
+          $rank = 1;
+
+          if (mysqli_num_row($result)) {
+            while ($row = mysqli_fetch_array($result)) {
+              echo "<td>{$rank}</td>
+              <td>{'name'}</td>
+              <td>{'score'}</td>";
+              $rank++
+            }
+          }
+        ?>
+      </table>
+    </body>
+</html>
+
+  

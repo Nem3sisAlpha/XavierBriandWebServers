bfc6451581eca259f91799f458f05cc0654b7e55
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-02 11:13:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,15 @@
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
         <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>
         </br></br>
-        <h5><h5>
+        <h4>Choose your game difficulty<h4>
+        <select>
+            <option value="1">Easy</option>
+            <option value="2">Medium</option>
+            <option value="3">Hard</option>
+            <option value="4">Custom</option>
+        </select> </br></br>
 
+            
     <form action="result.php" method="post">
 
         <div class="topnav">

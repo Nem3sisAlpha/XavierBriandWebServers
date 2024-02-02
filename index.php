0d64de33f9329f5e25d1c08a86c40c9844c37046
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-02 11:05:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
-        <h5>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h5>
+        <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>
         </br></br>
         <h5><h5>
 

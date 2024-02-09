60d05539f781bf30ba5cea97dd719bac0fc7be9c
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-09 12:53:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,33 +11,13 @@
         <form action="result.php" method="post">
             <h4>You may enter your name here</h4><input type="text" id="name" name="name" required> </br></br>
             <h4>You also may enter your email address here</h4> <input type="text" id="studentid" name="studentid" required> </br></br>
+
+        <input type="submit" value="Easy"><br><br>
+        <input type="submit" value="Medium"><br><br>
+        <input type="submit" value="Hard"><br><br>
+        <input type="submit" value="Custom"><br><br>
+            
         </form>
-        
-        <select id="optionSelect" onchange="visibility()">
-            <option value="Easy">Easy</option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
-            <option value="Custom">Custom</option>
-        </select>
-
-        <div id="customContent">
-            <h4>Content for Custom Option</h4>
-        </div>
-    
-        </form>
-
-        <script>
-            function visibility() {
-                var x = document.getElementById("optionSelect");
-                if (x.style.display === "Custom") {
-                    customContent.style.display = "block";
-                }
-                else {
-                    customContent.style.display = "none";
-                }
-            }
-        </script>
-
 
         
         <h4>For the Leaderboard, click here!</h4><br>

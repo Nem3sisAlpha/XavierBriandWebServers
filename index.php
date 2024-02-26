bc3bc214f3d8d21c739ed2d547a766a5c888f48c
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-26 01:27:43 -0500

    test

--- index.php
+++ index.php
@@ -41,10 +41,10 @@
         <script>
             function myFunction() {
                 var x = document.getElementById("myDIV");
-                if (x.style.display === "none") {
-                x.style.display = "block";
-                } else {
+                if (x.style.display === "block") {
                 x.style.display = "none";
+                } else {
+                x.style.display = "block";
                 }
             }
         </script>

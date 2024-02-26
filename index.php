23e307f1cb72a84bae963e29371f17beb96588bb
Nem3sisAlpha <xavierbriand31@gmail.com>
2024-02-26 00:15:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
         <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>
-        </br></br>
+        </br>
         
         <form action="result.php" method="post">
             <h4>You may enter your name here</h4><input type="text" id="name" name="name" required> </br></br>

c0cad9aaf552a1500d4a6d90902c51e2ed3b209d
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-26 01:30:15 -0500

    test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <style>
-            #myDIV {
+            #custom {
             display: none;  
             }
         </style>
@@ -13,6 +13,8 @@
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
         <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>
         </br>
+        
+        <div id="presets">
 
         <form action="result.php" method="post">
             <h4>You may enter your name here</h4><input type="text" id="name" name="name" required> </br></br>
@@ -22,13 +24,13 @@
         <input type="submit" id="diff" name="diff" value="Medium"><br><br>
         <input type="submit" id="diff" name="diff" value="Hard"><br><br>
         
-        
+        </div>
         
         </form>
 
         <button onclick="myFunction()">Custom</button><br><br>
 
-        <div id="myDIV">
+        <div id="custom">
             <input type="submit" id="diff" name="diff" value="Custom"><br><br>
         </div>
 
@@ -37,7 +39,7 @@
         
         <script>
             function myFunction() {
-                var x = document.getElementById("myDIV");
+                var x = document.getElementById("custom");
                 if (x.style.display === "block") {
                 x.style.display = "none";
                 } else {

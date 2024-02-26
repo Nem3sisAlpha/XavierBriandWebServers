c5a9f6d2ef3e69f76b75e2dc5b51f1e714e5da32
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-26 01:19:16 -0500

    test

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     </head>
     <body>
         
+        <script>
+            [myDIV].style.visibility='hidden'
+        </script>
 
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
         <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>

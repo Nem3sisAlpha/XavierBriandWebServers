a2d514db098ef6746615347d8f2a520592936867
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-02-26 01:36:17 -0500

    test

--- index.php
+++ index.php
@@ -12,11 +12,14 @@
 
         <h1>Welcome to the new revamped version of the Simon Says game!</h1>
         <h4>In here you will either be able to challenge or go easy on yourself as this game can be either really hard, or really easy depending on the settings you chose</h4>
-        <br>
+        </br>
         
         <div id="presets">
 
         <form action="result.php" method="post">
+            
+            <h3>It appears you want your game to have customized settings!</h3>
+
             <h4>You may enter your name here</h4><input type="text" id="name" name="name" required> </br></br>
             <h4>You also may enter your Student ID here</h4> <input type="text" id="studentid" name="studentid" required> </br></br>
 

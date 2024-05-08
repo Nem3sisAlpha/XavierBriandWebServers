3a2c7596dcbf9882c4d47810a4dc7fdf144e32ad
Nem3sisAlpha <75814944+Nem3sisAlpha@users.noreply.github.com>
2024-05-08 00:26:11 -0400

    test

--- custom_result.php
+++ custom_result.php
@@ -24,7 +24,7 @@
 
         $final_score = shell_exec("php /home/xavier/php/custom_settings.php $diff $timeint $timegiven $maxpatt");
 
-        $sql = "insert into leaderboard (name, score) values ($player, $final_score);";
+        $sql = "insert into leaderboard (name, score) values ('$player', '$final_score');";
         $result = mysqli_query($conn, $sql);
 
         ?>

e3f55751476950b2b079e4fb67bde9bbeea5a849
Nem3sisAlpha <xavierbriand31@gmail.com>
2024-05-16 20:07:54 -0400

    Small Result page update

--- custom_result.php
+++ custom_result.php
@@ -35,7 +35,7 @@
     <h3>Your student ID is: <?php echo $studentid; ?></h3><br>
     <h3>You set your difficulty to <?php echo $diff; ?></h3><br>
     <h3>You have <?php echo $timeint; ?> x50 milliseconds, for a grand total of <?php echo $int_res = $timeint * 50; ?> milliseconds between each generated pattern</h3><br>
-    <h3>You are given <?php echo $timegiven; ?> x100 milliseconds, for a grand total of <?php echo $give_res = $timegiven * 100; ?> to recreate the pattern</h3><br>
+    <h3>You are given <?php echo $timegiven; ?> x100 milliseconds, for a grand total of <?php echo $give_res = $timegiven * 100; ?> milliseconds to recreate the pattern</h3><br>
     <h3>You will have a squence of <?php echo $maxpatt; ?> patterns</h3><br><br>
     <h3>Your final score is: <?php echo $final_score; ?></h3><br><br>
 
